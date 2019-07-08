4345dd8e60ae0440f9121f82ce891df6e38518ec
Stefan <stefan.zivkovic995@gmail.com>
2019-07-08 14:47:16 +0200

    Revert "arm64: introduce is_device_dma_coherent"
    Not backported completely, revert it.
    
    This reverts commit e337c20ec2926ee8d9b6ae921f821ff6af556981.
    
    Signed-off-by: Zile995 <stefan.zivkovic995@gmail.com>

--- drivers/iommu/msm_dma_iommu_mapping.c
+++ drivers/iommu/msm_dma_iommu_mapping.c
@@ -193,11 +193,9 @@ static int __msm_dma_map_sg(struct device *dev, struct scatterlist *sg,
 		sg->dma_length = map->sgl.dma_length;
 
 		/*
-		 * Ensure all outstanding changes for coherent buffers are
-		 * applied to the cache before any DMA occurs.
+		 * Need to do cache operations here based on "dir" in the
+		 * future if we go with coherent mappings.
 		 */
-		if (is_device_dma_coherent(dev))
-			dmb(ish);
 	} else {
 		map = kmalloc(sizeof(*map), GFP_KERNEL);
 		if (!map) {
